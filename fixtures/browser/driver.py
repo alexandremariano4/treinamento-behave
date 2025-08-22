032cc443708e8c51c7e20430b05bf441785f94ea
Alexandre <alexandre_mariano@hotmail.com.br>
2025-08-22 19:30:33 -0300

    finalizando projeto

--- fixtures/browser/driver.py
+++ fixtures/browser/driver.py
@@ -1,4 +1,4 @@
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome,Remote
 from selenium.webdriver import ChromeOptions
 
 
@@ -7,10 +7,20 @@ def get_driver():
     # browser_options.add_argument('--headless=new')
     browser_options.add_argument('--log-level=3')
 
-    brw = Chrome(
-        options=browser_options
-    )
-    brw.implicitly_wait(10)
-    brw.maximize_window()
-    brw.set_window_size(1920,1080)
-    return brw
+    try:
+        browser_options.set_capability('browserName', 'chrome')
+        browser_options.set_capability('platformName', 'LINUX')
+        brw = Remote(
+            options=browser_options,
+            command_executor='http://localhost:4444'
+        )
+    except:
+        browser_options.capabilities.pop('platformName')
+        brw = Chrome(
+            options=browser_options
+        )
+    finally:
+        brw.maximize_window()
+        brw.set_window_size(1920,1080)
+        brw.implicitly_wait(10)
+        return brw
--- pages/HomePage.py
+++ pages/HomePage.py
@@ -5,7 +5,7 @@ from types import SimpleNamespace
 
 
 def validate_title(context):
-    context.brw.get('https://buger-eats.vercel.app/')
+    context.brw.get(context.config.userdata.get('BASE_URL'))
 
 def signup_button(context,text):
     context.click(f'//strong[text()="{text}"]','xpath')
